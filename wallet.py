a12137a60368a89f668378023cf3e955a80ccd5f
Yu-Cheng, Chang <45489725+Yucheng0208@users.noreply.github.com>
2025-04-27 21:25:17 +0800

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -4,8 +4,10 @@ class Wallet:
     def __init__(self, name):
         self.name = name
         self.address = str(uuid.uuid4())
-        self.balance = 100  # 初始碳權數量
-        self.stake = 0      # 初始質押量
+        self.balance = 100
+        self.stake = 0
+        self.transactions = []
+        self.mined_blocks = []
 
     def __repr__(self):
         return f"<Wallet {self.name}: {self.balance}CC, Stake: {self.stake}>"
