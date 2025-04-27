663edeacb676771c5f2e4f0d969d54c56aafd1ee
Yu-Cheng, Chang <45489725+Yucheng0208@users.noreply.github.com>
2025-04-27 21:25:13 +0800

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -6,18 +6,34 @@ from wallet import Wallet
 BLOCK_REWARD = 10
 TRANSACTION_FEE = 1
 
+def generate_hash(data):
+    return hashlib.sha256(str(data).encode()).hexdigest()
+
+class Transaction:
+    def __init__(self, sender, receiver, amount):
+        self.sender = sender
+        self.receiver = receiver
+        self.amount = amount
+        self.timestamp = time.time()
+        self.fee = TRANSACTION_FEE
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        data = f"{self.sender}{self.receiver}{self.amount}{self.timestamp}"
+        return generate_hash(data)
+
 class Block:
-    def __init__(self, index, timestamp, data, previous_hash, validator):
+    def __init__(self, index, timestamp, transactions, previous_hash, validator):
         self.index = index
         self.timestamp = timestamp
-        self.data = data
+        self.transactions = transactions
         self.previous_hash = previous_hash
         self.validator = validator
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        block_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}{self.validator}"
-        return hashlib.sha256(block_string.encode()).hexdigest()
+        block_string = f"{self.index}{self.timestamp}{[t.hash for t in self.transactions]}{self.previous_hash}{self.validator}"
+        return generate_hash(block_string)
 
 class Blockchain:
     def __init__(self):
@@ -26,7 +42,7 @@ class Blockchain:
         self.wallets = {}
 
     def create_genesis_block(self):
-        return Block(0, time.time(), "Genesis Block", "0", "admin")
+        return Block(0, time.time(), [], "0", "admin")
 
     def get_latest_block(self):
         return self.chain[-1]
@@ -38,14 +54,12 @@ class Blockchain:
         if self.wallets[sender].balance < (amount + TRANSACTION_FEE):
             raise ValueError("Insufficient balance including transaction fee.")
 
+        transaction = Transaction(sender, receiver, amount)
         self.wallets[sender].balance -= (amount + TRANSACTION_FEE)
         self.wallets[receiver].balance += amount
-        self.pending_transactions.append({
-            'sender': sender,
-            'receiver': receiver,
-            'amount': amount,
-            'fee': TRANSACTION_FEE
-        })
+        self.wallets[sender].transactions.append(transaction)
+        self.wallets[receiver].transactions.append(transaction)
+        self.pending_transactions.append(transaction)
 
     def stake(self, wallet_address, stake_amount):
         if wallet_address in self.wallets:
@@ -69,12 +83,10 @@ class Blockchain:
         if validator is None:
             return None
 
-        total_fee = sum(tx['fee'] for tx in self.pending_transactions)
-
         block = Block(
             index=len(self.chain),
             timestamp=time.time(),
-            data=self.pending_transactions,
+            transactions=self.pending_transactions.copy(),
             previous_hash=self.get_latest_block().hash,
             validator=validator
         )
@@ -82,7 +94,8 @@ class Blockchain:
 
         for name, wallet in self.wallets.items():
             if wallet.address == validator:
-                wallet.balance += BLOCK_REWARD + total_fee
+                wallet.balance += BLOCK_REWARD + sum(tx.fee for tx in self.pending_transactions)
+                wallet.mined_blocks.append(block.index)
 
         self.pending_transactions = []
         return block
